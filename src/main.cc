633e93bb111a2010e67e03dcd565a85acf7241a5
Wyatt Gill <101477459+wyattgill9@users.noreply.github.com>
2025-12-19 20:27:06 -0800

    .

--- src/main.cc
+++ src/main.cc
@@ -1,15 +1,69 @@
 #include <iostream>
+#include <print>
 
 #include <seastar/core/app-template.hh>
 #include <seastar/core/reactor.hh>
 
 #include "toolkit.hh"
 
-auto main(int argc, char** argv) -> i32 {
-    seastar::app_template app;
+struct DataPoint {
+    u64 time;
+    int data;
+};
 
-    app.run(argc, argv, [] {
-            std::cout << seastar::smp::count << "\n";
-            return seastar::make_ready_future<>();
-    });
+struct Series {
+private:
+    std::vector<DataPoint> points;
+
+public:
+    auto append(DataPoint point) {
+        points.push_back(point);
+    }
+
+    auto get_range(u64 start_time, u64 end_time) -> std::vector<DataPoint> {
+        std::vector<DataPoint> out;
+        for(auto& point : points) {
+            if(point.time >= start_time && point.time <= end_time) {
+                out.push_back(point);
+            }
+        }
+        return out;
+    }
+};
+
+seastar::future<> core() {
+    Series s;
+
+    DataPoint d {
+        .time = 100,
+        .data = 5
+    };
+
+    DataPoint p {
+        .time = 101,
+        .data = 6
+    };
+
+    s.append(d);
+    s.append(p);
+
+    auto range = s.get_range(100, 101);
+
+    for(auto& point : range) {
+        std::println("{}", point.data);
+    }
+
+    return seastar::make_ready_future<>();
+}
+
+auto main(int argc, char** argv) -> int {
+    seastar::app_template app;
+    try {
+        app.run(argc, argv, core);
+    } catch(...) {
+        std::cerr << "Couldn't start application: "
+                  << std::current_exception() << "\n";
+        return 1;
+    }
+    return 0;
 }
