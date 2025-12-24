e46d284f1f68f0dcb4cd19d39ddbecc0049e7f47
Wyatt Gill <wyattgill01@outlook.com>
2025-12-24 11:13:25 +0100

    .

--- src/main.cc
+++ src/main.cc
@@ -1,29 +1,94 @@
-#include <iostream>
-#include <print>
-
-#include <seastar/core/app-template.hh>
-#include <seastar/core/reactor.hh>
+// #include <seastar/core/app-template.hh>
+// #include <seastar/core/reactor.hh>
 
 #include "toolkit.hh"
 
-struct DataPoint {
-    u64 time;
-    int data;
-};
+#include <tuple>
+#include <type_traits>
+#include <unordered_map>
+#include <vector>
+#include <utility>
 
-struct Series {
+namespace tsdb {
+
+struct Time {
 private:
-    std::vector<DataPoint> points;
+    // year:    16 bits [0-15]   (allows 0-65535)
+    // month:   4 bits  [16-19]  (allows 0-15,  we use 1-12)
+    // day:     5 bits  [20-24]  (allows 0-31)
+    // hour:    5 bits  [25-29]  (allows 0-31,  we use 0-23)
+    // minute:  6 bits  [30-35]  (allows 0-63,  we use 0-59)
+    // second:  6 bits  [36-41]  (allows 0-63,  we use 0-59)
+    // weekday: 3 bits  [42-44]  (allows 0-7,   we use 1-7)
+    // yearday: 9 bits  [45-53]  (allows 0-511, we use 1-366)
+    // unused:  10 bits [54-63]
 
-public:
-    auto append(DataPoint point) {
-        points.push_back(point);
+    u64 bit_time;
+
+    // types: (position, width)
+    static constexpr auto YEAR    = std::pair{0,  16};
+    static constexpr auto MONTH   = std::pair{16, 4};
+    static constexpr auto DAY     = std::pair{20, 5};
+    static constexpr auto HOUR    = std::pair{25, 5};
+    static constexpr auto MINUTE  = std::pair{30, 6};
+    static constexpr auto SECOND  = std::pair{36, 6};
+    static constexpr auto WEEKDAY = std::pair{42, 3};
+    static constexpr auto YEARDAY = std::pair{45, 9};
+
+    [[nodiscard]] constexpr u64 get(std::pair<int, int> type) const {
+        auto [pos, width] = type;
+        return (bit_time >> pos) & ((1ULL << width) - 1);
     }
 
-    auto get_range(u64 start_time, u64 end_time) -> std::vector<DataPoint> {
-        std::vector<DataPoint> out;
+    constexpr u64 pack(std::pair<int, int> type, u64 val) const {
+        auto [pos, width] = type;
+        u64 mask = ((1ULL << width) - 1);
+        return (val & mask) << pos;
+    }
+
+public:
+    constexpr Time() : bit_time(0) {}
+    constexpr Time(u64 raw) : bit_time(raw) {}
+    
+    constexpr Time(u16 y, u8 mon, u8 d, u8 h = 0, u8 min = 0, u8 s = 0) 
+        : bit_time(
+              pack(YEAR, y)
+            | pack(MONTH, mon)
+            | pack(DAY, d)
+            | pack(HOUR, h)
+            | pack(MINUTE, min)
+            | pack(SECOND, s)
+        ) {}
+
+    [[nodiscard]] constexpr auto year   () const -> u64 { return get(YEAR   ); }
+    [[nodiscard]] constexpr auto month  () const -> u64 { return get(MONTH  ); }
+    [[nodiscard]] constexpr auto day    () const -> u64 { return get(DAY    ); }
+    [[nodiscard]] constexpr auto hour   () const -> u64 { return get(HOUR   ); }
+    [[nodiscard]] constexpr auto minute () const -> u64 { return get(MINUTE ); }
+    [[nodiscard]] constexpr auto second () const -> u64 { return get(SECOND ); }
+    [[nodiscard]] constexpr auto weekday() const -> u64 { return get(WEEKDAY); }
+    [[nodiscard]] constexpr auto yearday() const -> u64 { return get(YEARDAY); }
+
+    [[nodiscard]] constexpr auto inner() const -> u64 { return bit_time; }
+
+    [[nodiscard]] friend constexpr auto operator<=>(const Time&, const Time&) = default;
+};
+
+template<typename T>
+struct TimeSeries {
+private:
+    std::vector<T> points;
+
+public:
+    template<typename U>
+    auto append(U&& args) -> void {
+        points.emplace_back(std::forward<U>(args));
+    }
+
+    auto get_range( Time t1, Time t2 ) -> std::vector<T> {
+        std::vector<T> out;
         for(auto& point : points) {
-            if(point.time >= start_time && point.time <= end_time) {
+            if(point.time >= t1 && point.time <= t2) {
                 out.push_back(point);
             }
         }
@@ -31,39 +96,106 @@ public:
     }
 };
 
-seastar::future<> core() {
-    Series s;
+template<typename ...Ts>
+struct Segment {
+private:
+    // map (type) -> timeseries of (type)
+    std::tuple<TimeSeries<Ts>...> _series_map;
 
-    DataPoint d {
+public:
+    template<typename T>
+    constexpr inline auto get_series() -> TimeSeries<T>& {
+        return std::get<TimeSeries<T>>(_series_map);
+    }
+};
+
+template<typename T>
+concept isValidTimeSeries = requires (T t) {
+    { t.time   } -> std::convertible_to<Time>;
+    { t.active } -> std::convertible_to<bool>; // isPoolable
+};
+
+template<isValidTimeSeries... Ts>
+struct TSDB {
+private:
+    std::unordered_map<u64, Segment<Ts...>> _segment_map;
+
+public:
+    template<typename U>
+    auto append(U&& data_point) {
+        using T = std::remove_cvref_t<U>;
+       
+        Segment<Ts...>& seg = _segment_map.try_emplace(data_point.time.hour()).first->second;
+
+        seg
+            .template get_series<T>()
+            .append(std::forward<U>(data_point));
+    }
+
+    template<typename U>
+    auto get_range( Time t1, Time t2 ) -> std::vector<U> {
+        return {};
+    }
+};
+
+};
+
+// u64 is just simple time idk
+struct SimpleDataPoint : PoolObj {
+    sstring    name;
+    tsdb::Time time;
+
+    i32        data;
+};
+
+auto main() -> int {
+    tsdb::TSDB<SimpleDataPoint> t = {};
+
+    SimpleDataPoint d {
+        .name = "point_1",
         .time = 100,
-        .data = 5
+        .data = 5,  
     };
 
-    DataPoint p {
-        .time = 101,
-        .data = 6
-    };
+    t.append(d);
 
-    s.append(d);
-    s.append(p);
-
-    auto range = s.get_range(100, 101);
-
-    for(auto& point : range) {
-        std::println("{}", point.data);
-    }
-
-    return seastar::make_ready_future<>();
-}
-
-auto main(int argc, char** argv) -> int {
-    seastar::app_template app;
-    try {
-        app.run(argc, argv, core);
-    } catch(...) {
-        std::cerr << "Couldn't start application: "
-                  << std::current_exception() << "\n";
-        return 1;
-    }
     return 0;
 }
+
+// seastar::future<> core( {
+//     Series s;
+
+//     DataPoint d {
+//         .time = 100,
+//         .data = 5
+//     };
+
+//     DataPoint p {
+//         .time = 101,
+//         .data = 6
+//     };
+
+//     s.append(d);
+//     s.append(p);
+
+//     auto range = s.get_range(100, 101);
+
+//     for(auto& point : range) {
+//         std::println("{}", point.data);
+//     }
+
+//     return 0;
+//     return seastar::make_ready_future<>();
+// }
+
+// auto main(int argc, char** argv) -> int {
+//     seastar::app_template app;
+//     try {
+//         app.run(argc, argv, core);
+//     } catch(...) {
+//         std::cerr << "Couldn't start application: "
+//                   << std::current_exception() << "\n";
+//         return 1;
+//     }
+//     return 0;
+// }
--- src/toolkit.hh
+++ src/toolkit.hh
@@ -1,16 +1,157 @@
+// ( =< C++23 ) Dev toolkit
+
 #include <cstdint>
+#include <memory>
 #include <string>
+#include <concepts>
+#include <array>
+#include <cassert>
+#include <print>
+#include <type_traits>
+
+using size_type = size_t;
 
 using u64 = uint64_t;
 using u32 = uint32_t;
 using u16 = uint16_t;
+using u8  = uint8_t;
 
 using i64 = int64_t;
 using i32 = int32_t;
 using i16 = int16_t;
+using i8  = int8_t;
 
 using f64 = double;
 using f32 = float;
 
 using sstring      = std::string;
 using sstring_view = std::string_view;
+
+// Not for the optimization, but for the readibility
+#define LIKELY(x)   __builtin_expect(!!(x), 1)
+#define UNLIKELY(x) __builtin_expect(!!(x), 0)
+
+// MAP STUFF
+template<typename Map>
+concept isMap = requires {
+    typename Map::key_type;
+    typename Map::mapped_type;
+    typename Map::value_type;
+} && std::same_as<typename Map::value_type, std::pair<const typename Map::key_type, typename Map::mapped_type>>;
+
+template<isMap Map>
+constexpr inline auto get_or_create( Map& map, const typename Map::key_type& key) noexcept
+-> Map::value_type&& {
+    auto [it, inserted] = map.try_emplace(key);
+    return it->second;
+}
+
+// Meta
+
+template<typename ...T>
+struct TypeList {};
+
+template<template<typename> class F, typename List>
+struct Map;
+
+template<template<typename> class F, typename ...Ts>
+struct Map<F, TypeList<Ts...>> {
+    using type = TypeList<F<Ts>...>;
+};
+
+// --------------
+// ---- Pool ----
+// --------------
+
+// Inherit from:
+struct PoolObj {
+    bool active = false;
+};
+
+template<typename T>
+concept isPoolable = requires (T t) {
+    { t.active } -> std::convertible_to<bool>;
+};
+
+template<isPoolable T, size_t CAPACITY>
+class Pool {
+public:
+    using Handle = size_t;
+    static constexpr Handle INVALID_HANDLE = static_cast<Handle>(-1);
+
+    Pool() noexcept {
+        for(size_t i = 0; i < CAPACITY; ++i) {
+            _free_list[i] = static_cast<Handle>((CAPACITY - 1) - i);
+        }
+    }
+
+    ~Pool() {
+        for(auto& elem : _data) {
+            if(elem.active) {
+                elem.~T();
+            }
+        }
+    }
+
+    template<typename... Args>
+    [[nodiscard]] auto create( Args&&... args ) noexcept(std::is_nothrow_constructible_v<T, Args...>)
+    -> Handle {
+        if(UNLIKELY(_num_free == 0)) {
+            return INVALID_HANDLE;
+        }
+
+        const Handle index = _free_list[--_num_free];
+        
+        // perfect forwarding
+        std::construct_at(&_data[index], args... ); // new (&_data[index]) T {std::forward<Args>(args)...};
+        _data[index].active = true;
+
+        return index;
+    }
+
+    auto destroy( Handle handle ) noexcept
+    -> void {
+        if(UNLIKELY(handle >= CAPACITY || !_data[handle].active)) {
+            return;
+        }
+
+        std::destroy_at(&_data[handle]); // _data[handle].~T();
+        _data[handle].active = false;
+        _free_list[_num_free++] = handle;
+    }
+
+    [[nodiscard]] auto get( Handle handle ) noexcept
+    -> T* {
+        if(UNLIKELY(handle >= CAPACITY || !_data[handle].active)) {
+            std::println("UNABLE TO RETRIEVE POOL OBJ (id: {}) ", handle);
+            return nullptr;
+        }
+        return &_data[handle];
+    }
+
+    [[nodiscard]] auto operator[]( Handle handle ) noexcept
+    -> T& {
+        assert(handle < CAPACITY && _data[handle].active);
+        return _data[handle];
+    }
+
+    template<typename Func>
+    auto for_each( Func&& func )
+    -> void {
+        for(auto& elem : _data) {
+            if(LIKELY(elem.active)) {
+                func(elem);
+            }
+        }
+    }
+
+    [[nodiscard]] size_t size    () const noexcept { return CAPACITY - _num_free; }
+    [[nodiscard]] size_t capacity() const noexcept { return CAPACITY; }
+    [[nodiscard]] bool   full    () const noexcept { return _num_free == 0; }
+    [[nodiscard]] bool   empty   () const noexcept { return _num_free == CAPACITY; }
+
+private:
+    std::array<T, CAPACITY>      _data;
+    std::array<Handle, CAPACITY> _free_list;
+    size_t                       _num_free = CAPACITY;
+};
